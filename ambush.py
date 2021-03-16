2e1ab8f2a5e784007647d9390ceafef74a2a20ca
O46 <alec.ritter@gmail.com>
2021-03-16 17:40:56 -0400

    Big improvements
    Added secondary seed that will remain consistent between runs, even when only a single seed is given.
    Changed where the randomization occurs
    Fixed error with file data being maintained

--- ambush.py
+++ ambush.py
@@ -1,13 +1,13 @@
 import os
 import random
 import shutil
-
+import math
 
 class iteite:
     def __init__(self, container_directory):
         self.container_directory = container_directory
         
-    def item_iterator(self, user_dir, boss_skip, rand_seed=0):
+    def item_iterator(self, user_dir, boss_skip, num_seed, mon_seed):
         full_list = os.listdir(user_dir)
         clean_list = []
         number_list = []
@@ -17,7 +17,7 @@ class iteite:
         with open("files to ignore", "r") as ignore_list:
             ignored_numbers = ignore_list.readlines()
 
-        if boss_skip == True:
+        if boss_skip:
             with open("bosses", "r") as boss_list:
                 ignored_numbers.extend(boss_list.readlines())
 
@@ -27,19 +27,22 @@ class iteite:
 
         for file in clean_list:
             number_list.append(file.split("-")[0])
-            name_list.append(file.split("-")[1])
+            name_list.append(file.split("-", 1)[1])
 
-        random.Random(rand_seed).shuffle(name_list)
+        random.Random(num_seed).shuffle(number_list)
 
-        for item in number_list:
-            comb_list.append(item + "-" + name_list[number_list.index(item)])
-
-        i = 0	
+        i = 0
         for item in clean_list:
-        	shutil.move(os.path.join(user_dir, item), os.path.join(user_dir, comb_list[i]))
-        	i += 1
+                comb_list.append(number_list[i] + "-" + name_list[i])
+                i+=1
 
-        print("Seed used: ", rand_seed, "\n\n\n")
+        i = 0
+        for item in clean_list:
+            shutil.move(os.path.join(user_dir, item), os.path.join(user_dir, comb_list[i]))
+            i += 1
+
+        post_list = os.listdir(user_dir)
+        print("Seed used: ", num_seed, "\n\n\n")
 
 
 if __name__ == "__main__":
@@ -49,12 +52,13 @@ if __name__ == "__main__":
     while True:
         try:
             user_dir = input("Input the folder you would like to modify: ").strip()
-            rand_seed = input("Enter a seed for randomization or leave empty: ").strip()
+            num_seed = input("Enter a seed for randomization or leave empty: ").strip()
             boss_skip = input("Randomize bosses? (y/n): ").strip().lower()[0]
             if boss_skip == "y":
                 boss_skip = True
-            if len(rand_seed) < 1:
-                rand_seed = random.getrandbits(64)
-            ite.item_iterator(user_dir, boss_skip, rand_seed)
+            if len(num_seed) < 1:
+                num_seed = random.getrandbits(64)
+            mon_seed = math.floor(float(num_seed) * .46) + 46
+            ite.item_iterator(user_dir, boss_skip, num_seed, mon_seed)
         except Exception as e:
             print("Error: {0}\nPlease try again.".format(e))
