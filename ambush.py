42f272d98407f777f28cdd639ea6bee09a7757fe
O46 <alec.ritter@gmail.com>
2021-03-15 17:04:58 -0700

    Add files via upload
    Boss files can be changed in the bosses folder, general empty items can be added in the "files to ignore" file
    Needs a base Python 3.7 install
    Make sure the folder your items are in is in the same folder as the script

--- ambush.py
+++ ambush.py
@@ -1,18 +1,45 @@
 import os
+import random
+import shutil
+
 
 class iteite:
     def __init__(self, container_directory):
         self.container_directory = container_directory
         
-    def item_iterator(self, user_dir, rand_seed=0):
+    def item_iterator(self, user_dir, boss_skip, rand_seed=0):
         full_list = os.listdir(user_dir)
-
+        clean_list = []
         number_list = []
         name_list = []
+        comb_list = []
 
-        for file in full_list:
-        	print(file)
-        	print(file.split("-"))
+        with open("files to ignore", "r") as ignore_list:
+            ignored_numbers = ignore_list.readlines()
+
+        if boss_skip == True:
+            with open("bosses", "r") as boss_list:
+                ignored_numbers.extend(boss_list.readlines())
+
+        for item in full_list:
+            if not any(bad_number.strip() in item for bad_number in ignored_numbers):
+                clean_list.append(item)
+
+        for file in clean_list:
+            number_list.append(file.split("-")[0])
+            name_list.append(file.split("-")[1])
+
+        random.Random(rand_seed).shuffle(name_list)
+
+        for item in number_list:
+            comb_list.append(item + "-" + name_list[number_list.index(item)])
+
+        i = 0	
+        for item in clean_list:
+        	shutil.move(os.path.join(user_dir, item), os.path.join(user_dir, comb_list[i]))
+        	i += 1
+
+        print("Seed used: ", rand_seed, "\n\n\n")
 
 
 if __name__ == "__main__":
@@ -20,8 +47,14 @@ if __name__ == "__main__":
     ite = iteite(cur_dir)
     print(" Etrian Randomizer\n", "-"*20, "\n")
     while True:
-        user_dir = input("Input the folder you would like to modify: ").strip()
-        rand_seed = input("Enter a seed for randomization or leave empty: ").strip()
-        if len(rand_seed) < 0:
-            rand_seed = random.getrandbits(64)
-        ite.item_iterator(user_dir, rand_seed)
+        try:
+            user_dir = input("Input the folder you would like to modify: ").strip()
+            rand_seed = input("Enter a seed for randomization or leave empty: ").strip()
+            boss_skip = input("Randomize bosses? (y/n): ").strip().lower()[0]
+            if boss_skip == "y":
+                boss_skip = True
+            if len(rand_seed) < 1:
+                rand_seed = random.getrandbits(64)
+            ite.item_iterator(user_dir, boss_skip, rand_seed)
+        except Exception as e:
+            print("Error: {0}\nPlease try again.".format(e))
